67a2fb6c792d2c6b442a8f9015673ec175a29686
marius <marius.franck@uphf.fr>
2022-04-05 16:30:49 +0200

    Commit test V2 Terraria Marius

--- Terraria/src/CodeV2/Eau.java
+++ Terraria/src/CodeV2/Eau.java
@@ -4,4 +4,12 @@ public class Eau {
     private int x;
     private int y;
     private boolean eau;
+    private Monde monde;
+
+    public Eau(boolean eau, int x,int y, Monde){
+        this.eau=eau;
+        this.x=x;
+        this.y=y;
+        this.monde=Monde;
+    }
 }
--- Terraria/src/CodeV2/Monde.java
+++ Terraria/src/CodeV2/Monde.java
@@ -23,9 +23,86 @@ public class Monde {
         return element;
     }
     public void Generation_Map{
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        Random ran= new Random();
+        int water=1;
+        int entr=1;
+        int rob=1;
+        int m=1;
+        boolean b;
+        int[] tab=new int[6];
+        tab[0]=ran.nextInt(11);
+        tab[1]=ran.nextInt(5)+1;
+        tab[2]=ran.nextInt(5)+1;
+        tab[3]=ran.nextInt(2)+1;
+        tab[4]=ran.nextInt(2)+1;
+        tab[5]=2;
+        for (int i=0; i< tab.length; i++){
+            if (i=0){
+                for (j=0;j<=tab[i];j++){
+                    int px=ran.nextInt(11);
+                    int py=ran.nextInt(11);
+                    terrain[px][py][0]=new Eau(true,px,py,this);
+                    terrain[px][py][1]=new Eau(true,px,py,this);
+                }
+            }
+            else if (i=1){
+                for (j=0;j<=tab[i];j++){
+                    int px=ran.nextInt(11);
+                    int py=ran.nextInt(11);
+                    terrain[px][py][0]=new Eau(false,px,py,this);
+                    terrain[px][py][1]=new Robot(true,px,py,this);
+                }
+            }
+            else if (i=2){
+                for (j=0;j<=tab[i];j++){
+                    int px=ran.nextInt(11);
+                    int py=ran.nextInt(11);
+                    terrain[px][py][0]=new Eau(false,px,py,this);
+                    terrain[px][py][1]=new Robot(false,px,py,this);
+                }
+            }
+            else if (i=3){
+                for (j=0;j<=tab[i];j++){
+                    int px=ran.nextInt(11);
+                    int py=ran.nextInt(11);
+                    terrain[px][py][0]=new Mine(true,px,py,this);
+                    terrain[px][py][1]=new Eau(false,px,py,this);
+                }
+            }
+            else if (i=4){
+                for (j=0;j<=tab[i];j++){
+                    int px=ran.nextInt(11);
+                    int py=ran.nextInt(11);
+                    terrain[px][py][0]=new Mine(false,px,py,this);
+                    terrain[px][py][1]=new Eau(false,px,py,this);
+                }
+            }
+            else if (i=5){
+                for (j=0;j<=tab[i];j++){
+                    if(j=0) {
+                        int px = ran.nextInt(11);
+                        int py = ran.nextInt(11);
+                        terrain[px][py][0] = new Entrepot(true, px, py, this);
+                        terrain[px][py][1] = new Eau(false, px, py, this);
+                    }
+                    else{
+                        int px = ran.nextInt(11);
+                        int py = ran.nextInt(11);
+                        terrain[px][py][0] = new Entrepot(false, px, py, this);
+                        terrain[px][py][1] = new Eau(false, px, py, this);
+                    }
+                }
             }
         }
+        for (int i=0;i<terrain.length;i++){
+            for (int j=0;j<terrain.length[0];j++) {
+                for (int k = 0; k < terrain[0][0].length; k++) {
+                    if(terrain[i][j][k]==null){
+                        terrain[i][j][k]==new Eau(false,i,j,this)
+                    }
+                }
+            }
+        }
+
     }
 }
