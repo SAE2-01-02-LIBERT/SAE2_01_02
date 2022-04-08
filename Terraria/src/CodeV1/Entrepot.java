6cf58baeeacab06a9c7451dae521ef5085c7763b
LIBERT Quentin <79151732+Hychion@users.noreply.github.com>
2022-04-08 22:06:07 +0200

    Update Entrepot.java

--- Terraria/src/CodeV1/Entrepot.java
+++ Terraria/src/CodeV1/Entrepot.java
@@ -1,11 +1,25 @@
 package CodeV1;
 
-import java.util.*;
-
-public class Entrepot extends Terre {
+public class Entrepot extends Batiment {
     private int stock;
     private String type;
+    private int numero;
 
-    public Entrepot() {
+    public Entrepot(String t, int n) {
+        this.stock = 0;
+        this.type = t;
+        this.numero = n;
+    }
+    public int getstock() {
+        return this.stock;
+    }
+
+    @Override
+    public String getInfo() {
+        return "E " + this.numero + "\n\t" + this.type + this.stock + "/" ;
+    }
+
+    public int getStock() {
+        return this.stock;
     }
 }
