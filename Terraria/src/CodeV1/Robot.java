f1adffa32a9d6b872f749b5e30941c4dcef2cffc
LIBERT Quentin <79151732+Hychion@users.noreply.github.com>
2022-04-08 22:08:06 +0200

    Update Robot.java

--- Terraria/src/CodeV1/Robot.java
+++ Terraria/src/CodeV1/Robot.java
@@ -3,16 +3,42 @@ package CodeV1;
 import java.util.*;
 
 public class Robot extends Batiment {
-    private String type;
+    private static String type;
     private int soute;
     private int capacitee;
+    private int numero;
+    private int[] position;
 
-    public Robot() {
+    public Robot(String type, int[] coord, int num) {
+        this.type = type;
+        this.numero = num;
+        this.soute = 0;
+        this.capacitee = 0;
+        this.position[0]=coord[0];
+        this.position[1]=coord[1];
+    }
+    public Robot(Robot r,int[]Npos){
+        this.soute=r.soute;
+        this.position=Npos;
+        this.capacitee=r.capacitee;
+        this.numero=r.numero;
+        this.type=r.type;
     }
 
     public void recolter() {
         // TODO implement here
     }
+
+    public void deposer() {
+        // TODO implement here
+    }
+
+    /**
+     *partie des deplacements
+     * Attention on doit faire gaffe a ce que deux robot ne soit pas dans
+     * le meme secteur
+     * Et que le robot ne sorte pas de la map sa serait problemeatique.
+     */
 
     public void nord() {
         // TODO implement here
@@ -30,7 +56,14 @@ public class Robot extends Batiment {
         // TODO implement here
     }
 
-    public void deposer() {
-        // TODO implement here
+
+    @Override
+    public String getInfo() {
+        return "" + "R" + this.numero + "\n" +"\t"+ this.type + this.soute + "/" + this.soute;
+    }
+
+    @Override
+    public int getStock() {
+        return this.soute;
     }
 }
