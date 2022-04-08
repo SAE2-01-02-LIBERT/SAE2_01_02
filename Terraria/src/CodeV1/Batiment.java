9ad5be17758e87f1d285c3bf108a65a7a2a69ca2
Hychion_ <quentinlibert59@gmail.com>
2022-04-09 00:33:52 +0200

    last for today

--- Terraria/src/CodeV1/Batiment.java
+++ Terraria/src/CodeV1/Batiment.java
@@ -1,6 +1,6 @@
 package CodeV1;
 
 public abstract class Batiment implements Localisable {
-    protected abstract String getInfo();
-    protected abstract int getStock();
+    public abstract String getInfo();
+    public abstract int getStock();
 }
