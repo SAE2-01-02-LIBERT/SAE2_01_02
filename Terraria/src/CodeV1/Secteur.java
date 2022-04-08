4fb3803c72daf8848f1a526a80cc7d749ddb9993
LIBERT Quentin <79151732+Hychion@users.noreply.github.com>
2022-04-08 22:08:46 +0200

    Update Secteur.java

--- Terraria/src/CodeV1/Secteur.java
+++ Terraria/src/CodeV1/Secteur.java
@@ -1,9 +1,56 @@
 package CodeV1;
 
-public abstract class Secteur {
-    protected int[] coodonee;
+import java.util.concurrent.ExecutionException;
 
-    public abstract int[] getposition();
-    public abstract String gettype();
+public class Terre extends Secteur{
+    private String type;
+    private Localisable[] locals;
 
+    public Terre() {
+        this.type = "Terre";
+        this.locals = new Localisable[2];
+    }
+
+    public void ajoutLocalR(Robot r)throws ExecutionException{
+        try {
+            if (this.locals[1] == null) {
+                r = new Robot(r, getposition());
+                this.locals[1] = r;
+            }
+        }
+        finally {
+            System.out.println("Deplacement impossible, un robot est deja dans le secteur !! en  / "+ this.getposition() );
+        }
+    }
+
+    public void ajoutLocalM(Mine m) throws ExecutionException {
+        try {
+            if (this.locals[0] == null) {
+                this.locals[0] = m;
+            }
+        } finally {
+            System.out.println("Implementation Imposible de la Mine  un element du meme type deja present / " + this.getposition());
+        }
+    }
+
+    public void ajoutLocalE(Entrepot emp) throws ExecutionException{
+        try {
+            if (this.locals[0] == null) {
+                this.locals[0] = emp;
+            }
+        }
+        finally{
+            System.out.println("Implementation Imposible de l'entrepot  un element du meme type deja present/ " + this.getposition());
+            }
+        }
+
+    @Override
+    public int[] getposition() {
+        return this.coodonee;
+    }
+
+    @Override
+    public String gettype(){
+        return this.type;
+    }
 }
