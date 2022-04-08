0b1cc068bdfaece5699ea6218d66e367e0c7e1d9
LIBERT Quentin <79151732+Hychion@users.noreply.github.com>
2022-04-08 22:07:40 +0200

    Update Monde.java

--- Terraria/src/CodeV1/Monde.java
+++ Terraria/src/CodeV1/Monde.java
@@ -1,16 +1,39 @@
 package CodeV1;
 
-import java.util.*;
-
 public class Monde {
-    private Secteur[] monde;
+    private Secteur[][] monde;
+    private Robot[] roboList;
+    private Mine[] mineList;
+    private Entrepot[] entrepotList;
 
     public Monde() {
+        this.entrepotList=new Entrepot[2];
+        this.mineList = new Mine[2];
+        this.roboList=new Robot[2];
+        this.monde = new Secteur[10][10];
     }
 
-    public void creerMonde() {
-        // TODO implement here
+
+    public void creerMonde(){
+        int eau = 0;
+        int[] coor = new int[1];
+        for (int i = 0 ; i < 9 ;i++){
+            coor[0]=i;
+            for (int j = 0 ; j < 9 ; j++){
+                coor[1]=j;
+                int valeur = (int)Math.random();
+                if (valeur == 1 || eau == 10){
+                    this.monde[i][j]= new Terre();
+                }
+                else{
+                    this.monde[i][j]=new Eau();
+                    eau += 1;
+                }
+            }
+        }
+
     }
+
 
     public void affichermonde() {
         // TODO implement here
