dbb64b911ab152aacc8f14521883a6cd157102b3
LIBERT Quentin <79151732+Hychion@users.noreply.github.com>
2022-04-08 22:08:29 +0200

    Update Secteur.java

--- Terraria/src/CodeV1/Secteur.java
+++ Terraria/src/CodeV1/Secteur.java
@@ -1,19 +1,9 @@
 package CodeV1;
 
-import java.util.*;
-
 public abstract class Secteur {
-    private int[] coodonee;
+    protected int[] coodonee;
 
-    public Secteur() {
-    }
-
-    public void getposition() {
-        // TODO implement here
-    }
-
-    public void getType() {
-        // TODO implement here
-    }
+    public abstract int[] getposition();
+    public abstract String gettype();
 
 }
