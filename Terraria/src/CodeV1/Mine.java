1c7bfcab84e4ce9bbfb97ce02713ba61d1b62fca
LIBERT Quentin <79151732+Hychion@users.noreply.github.com>
2022-04-08 22:34:15 +0200

    Update Mine.java

--- Terraria/src/CodeV1/Mine.java
+++ Terraria/src/CodeV1/Mine.java
@@ -1,56 +1,26 @@
 package CodeV1;
 
-import java.util.concurrent.ExecutionException;
+public class Mine extends Batiment {
+    private final int numero;
+    private final String type;
+    private int capacite;
+    private int stock;
 
-public class Terre extends Secteur{
-    private String type;
-    private Localisable[] locals;
-
-    public Terre() {
-        this.type = "Terre";
-        this.locals = new Localisable[2];
-    }
-
-    public void ajoutLocalR(Robot r)throws ExecutionException{
-        try {
-            if (this.locals[1] == null) {
-                r = new Robot(r, getposition());
-                this.locals[1] = r;
-            }
-        }
-        finally {
-            System.out.println("Deplacement impossible, un robot est deja dans le secteur !! en  / "+ this.getposition() );
-        }
-    }
-
-    public void ajoutLocalM(Mine m) throws ExecutionException {
-        try {
-            if (this.locals[0] == null) {
-                this.locals[0] = m;
-            }
-        } finally {
-            System.out.println("Implementation Imposible de la Mine  un element du meme type deja present / " + this.getposition());
-        }
-    }
-
-    public void ajoutLocalE(Entrepot emp) throws ExecutionException{
-        try {
-            if (this.locals[0] == null) {
-                this.locals[0] = emp;
-            }
-        }
-        finally{
-            System.out.println("Implementation Imposible de l'entrepot  un element du meme type deja present/ " + this.getposition());
-            }
-        }
-
-    @Override
-    public int[] getposition() {
-        return this.coodonee;
+    public Mine(String typ, int num, String type, int stk){
+        this.type = typ;
+        this.stock=stk;
+        this.numero=num;
+        this.capacite=(int)(50+Math.random() * (double)(50-0));
     }
 
     @Override
-    public String gettype(){
-        return this.type;
+    public String getInfo() {
+        return "M " + this.numero +"\n\t" +this.stock +"/" +this.capacite;
     }
+
+    @Override
+    public int getStock() {
+        return this.stock;
+    }
+
 }
