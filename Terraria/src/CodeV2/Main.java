ed85e206c0eb5a0aadbcdaab17811f487d64319d
marius <marius.franck@uphf.fr>
2022-04-29 10:46:03 +0200

    Affichage

--- Terraria/src/CodeV2/Main.java
+++ Terraria/src/CodeV2/Main.java
@@ -1,4 +1,12 @@
 package CodeV2;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Monde world = new Monde(1);
+        world.creer_monde();
+        world.afficher_console(0);
+
+
+    }
 }
