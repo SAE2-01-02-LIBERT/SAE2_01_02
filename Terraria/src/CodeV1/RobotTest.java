718ffd65fd18818dcea8299de8550303f21ad57a
Eddy Du Bois <eddy.dubois.59600@gmail.com>
2022-05-11 16:08:59 +0200

    JUnit test

--- Terraria/src/CodeV1/RobotTest.java
+++ Terraria/src/CodeV1/RobotTest.java
@@ -3,7 +3,7 @@ package CodeV1;
 import CodeV1.Robot;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RobotTest {
     protected Robot Rb;
@@ -18,7 +18,7 @@ class RobotTest {
     }
 
     @Test
-    void test1c (){
+    void InfCapacite (){
         Rb.setCapacitee(5);
         Rb.setSoute(0);
         Rb.setCapaciteeExtraction(1);
@@ -29,13 +29,23 @@ class RobotTest {
     }
 
     @Test
-    void test2c (){
+    void SupCapacite (){
         Rb.setCapacitee(5);
         Rb.setSoute(4);
         Rb.setCapaciteeExtraction(2);
         int val = Rb.getStock();
         int cpE = Rb.getCapaciteeExtraction();
         Rb.recolter();
-        assertEquals(val+cpE, Rb.getStock());
+        assertNotEquals(val+cpE, Rb.getStock());
+    }
+
+    @Test
+    void CapSouteEgal (){
+        Rb.setCapacitee(5);
+        Rb.setSoute(5);
+        Rb.setCapaciteeExtraction(1);
+        int val = Rb.getStock();
+        Rb.recolter();
+        assertEquals(val, Rb.getStock());
     }
 }
