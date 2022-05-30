6408a3a8e2f36b1099e57dd13403122a466e2b02
HaronMan <102169938+HaronMan@users.noreply.github.com>
2022-05-30 14:49:50 +0200

    Update FenetreScore.java
    update fenetrescore

--- TerrariaFX/src/main/java/com/example/terrariafx/FenetreScore.java
+++ TerrariaFX/src/main/java/com/example/terrariafx/FenetreScore.java
@@ -1,11 +1,17 @@
 package com.example.terrariafx;
 
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
@@ -26,8 +32,9 @@ public class FenetreScore extends Stage {
         this.tours = tours;
 
         this.setTitle("Fin de partie");
-        Label go = new Label("GAME OVER");
-        Label sf = new Label("Score final :");
+        Label go = new Label("GAME OVER!!!\n\n");
+        Label sf = new Label("Score final : ");
+        Label score = new Label(""+(this.orRecup+this.niRecup-this.tours));
         Label or1 = new Label("Or récupéré : "+this.orRecup);
         Label or2 = new Label("Or restant : "+this.orRestant);
         Label ni1 = new Label("Nickel récupéré : "+this.niRecup);
@@ -35,13 +42,14 @@ public class FenetreScore extends Stage {
         Label t = new Label("Nombre de tours joué : "+this.tours);
         Button quit = new Button("Quitter");
 
-        go.setTextAlignment(TextAlignment.CENTER);
-        go.setMinHeight(40);
-        go.setMaxHeight(40);
+        sf.setWrapText(true);
+        go.setFont(new Font("Arial", 21));
+        score.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
         VBox vbox = new VBox();
         vbox.getChildren().add(go);
         vbox.getChildren().add(sf);
+        vbox.getChildren().add(score);
         vbox.getChildren().add(or1);
         vbox.getChildren().add(or2);
         vbox.getChildren().add(ni1);
@@ -57,7 +65,14 @@ public class FenetreScore extends Stage {
             }
         });
 
-        Scene scene = new Scene(vbox, 160, 200);
+        vbox.setPadding(new Insets(0,20,20, 10));
+
+        Group root = new Group();
+        ImageView font = new ImageView(new Image("end.png"));
+        root.getChildren().add(font);
+        root.getChildren().add(vbox);
+
+        Scene scene = new Scene(root, 160, 200);
         this.setScene(scene);
         this.show();
     }
