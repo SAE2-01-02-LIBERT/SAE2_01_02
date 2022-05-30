2bd7a9ad018eff225e365109ddca48979b8e7355
HaronMan <102169938+HaronMan@users.noreply.github.com>
2022-05-30 14:48:31 +0200

    Update GestionEventActionRobot.java
    update de la sélection des robots

--- TerrariaFX/src/main/java/com/example/terrariafx/GestionEventActionRobot.java
+++ TerrariaFX/src/main/java/com/example/terrariafx/GestionEventActionRobot.java
@@ -2,6 +2,7 @@ package com.example.terrariafx;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 
 import java.util.concurrent.ExecutionException;
 
@@ -10,12 +11,14 @@ public class GestionEventActionRobot implements EventHandler<KeyEvent> {
     private final Monde monde; //redondance d'info
     private String Rbsect;
     private Robot rob;
+    private Text afficherRobot;
 
-    public GestionEventActionRobot(Monde monde, Jeu jeu, Robot r) {
+    public GestionEventActionRobot(Monde monde, Jeu jeu, Robot r, Text aR) {
         this.monde = monde;
         this.jeu = jeu;
         this.Rbsect = "&";
         this.rob = r;
+        this.afficherRobot = aR;
     }
 
     @Override
@@ -24,59 +27,80 @@ public class GestionEventActionRobot implements EventHandler<KeyEvent> {
             this.Rbsect= k.getText();
             rob = monde.getRoboList()[0];
             jeu.setRbD(rob);
+            afficherRobot = new Text("Robot selectionne : 1");
+
         } else if (k.getText().equals("é")) {
             this.Rbsect= k.getText();
             rob = monde.getRoboList()[1];
             jeu.setRbD(rob);
+            afficherRobot = new Text("Robot selectionne : 2");
+
         } else if (k.getText().equals(Character.toString((char) 34))) {
             if (monde.getNbrRobot() >= 3) {
                 this.Rbsect= k.getText();
                 rob = monde.getRoboList()[2];
                 jeu.setRbD(rob);
+                afficherRobot = new Text("Robot selectionne : 3");
             }
+
         } else if (k.getText().equals("'")) {
             if (monde.getNbrRobot() >= 4) {
                 this.Rbsect= k.getText();
                 rob = monde.getRoboList()[3];
                 jeu.setRbD(rob);
+                afficherRobot = new Text("Robot selectionne : 4");
             }
+
         } else if (k.getText().equals("(")) {
             if (monde.getNbrRobot() >= 5) {
                 this.Rbsect= k.getText();
                 rob = monde.getRoboList()[4];
                 jeu.setRbD(rob);
+                afficherRobot = new Text("Robot selectionne : 5");
             }
+
         } else if (k.getText().equals("-")) {
             if (monde.getNbrRobot() >= 6) {
                 this.Rbsect= k.getText();
                 rob = monde.getRoboList()[5];
                 jeu.setRbD(rob);
+                afficherRobot = new Text("Robot selectionne : 6");
             }
+
         } else if (k.getText().equals("è")) {
             if (monde.getNbrRobot() >= 7) {
                 this.Rbsect= k.getText();
                 rob = monde.getRoboList()[6];
                 jeu.setRbD(rob);
+                afficherRobot = new Text("Robot selectionne : 7");
             }
+
         } else if (k.getText().equals("_")) {
             if (monde.getNbrRobot() >= 8) {
                 this.Rbsect= k.getText();
                 rob = monde.getRoboList()[7];
                 jeu.setRbD(rob);
+                afficherRobot = new Text("Robot selectionne : 8");
             }
+
         } else if (k.getText().equals("ç")) {
             if (monde.getNbrRobot() >= 9) {
                 this.Rbsect= k.getText();
                 rob = monde.getRoboList()[8];
                 jeu.setRbD(rob);
+                afficherRobot = new Text("Robot selectionne : 9");
             }
+
         } else if (k.getText().equals("à")) {
             if (monde.getNbrRobot() >= 10) {
-                this.Rbsect= k.getText();
+                this.Rbsect = k.getText();
                 rob = monde.getRoboList()[9];
                 jeu.setRbD(rob);
+                afficherRobot = new Text("Robot selectionne : 10");
             }
-        } else if (k.getText().equals("z")) {
+        }
+        jeu.actualiserinfop();
+        if (k.getText().equals("z")) {
             try {
                 monde.actionRobot(jeu.getRbD(), "N");
                 jeu.getMondeGUI().setGrille();
